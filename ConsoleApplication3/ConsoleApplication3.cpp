c0700888f09193425a906835ffa2b81124550671
GitHubmaster420 <joonavjs7@hotmail.fi>
2020-09-24 21:16:07 +0100

    cpp updated

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -6,16 +6,16 @@
 
 using namespace std;
 
-void Tt1();
-void Tt2();
-void Tt3();
-void Tt4();
-void Tt5();
-void Vt1();
-void Vt2();
-void Vt3();
-void Vt4();
-void Vt5();
+void TuntiTeht1();
+void TuntiTeht2();
+void TuntiTeht3();
+void TuntiTeht4();
+void TuntiTeht5();
+void ViikkoTeht1();
+void ViikkoTeht2();
+void Viikkoteht3();
+void ViikkoTeht4();
+void ViikkoTeht5();
 
 /*
 main-funktio on ohjelman aloituspiste, ohjelmissa on yleensä useampia funktioita,
@@ -28,20 +28,10 @@ Tehtava2() { aaltosulkeiden väliin }
 
 int main()
 {
-	Tt1();
-	Tt2();
-	Tt3();
-	Tt4();
-	Tt5();
-	Vt1();
-	Vt2();
-	Vt3();
-	Vt4();
-	Vt5();
 }
 
 
-void Tt1() {
+void TuntiTeht1() {
 
 	int luku1;
 
@@ -69,7 +59,7 @@ void Tt1() {
 
 }
 
-void Tt2() {
+void TuntiTeht2() {
 
 	int inputdays;
 
@@ -89,7 +79,7 @@ void Tt2() {
 
 }
 
-void Tt3() {
+void TuntiTeht3() {
 	/*Kirjoita edellisen kaltainen ohjelma, joka muuntaa käyttäjän antamat sekunnit kokonaisiksi tunneiksi, minuuteiksi ja sekunneiksi muotoon H:M:S:
 
 Input seconds: 25300                                                   
@@ -109,7 +99,7 @@ Leading zero -muotoiluja ei tarvitse tehdä, mutta halutessaan sellaisen saa aika
 
 }
 
-void Tt4() {
+void TuntiTeht4() {
 
 
 	int pisteet;
@@ -124,7 +114,10 @@ void Tt4() {
 		arvosana = (pisteet - 40) / 10;
 	}
 	else if (pisteet == 100) { arvosana = 5; }
-	else { cout << "pisteet ei välillä 0 - 100" << endl; }
+	else {
+		cout << "pisteet ei välillä 0 - 100" << endl;
+		arvosana = -100;
+	}
 
 	cout << "arvosana:" << arvosana << endl;
 	if (pisteet == 100) {
@@ -132,7 +125,7 @@ void Tt4() {
 	}
 }
 
-void Tt5() {
+void TuntiTeht5() {
 	/*Sitten "hauska" Intialainen ohjelmointitehtävä :
 
 	Kirjoita ohjelma joka pyytää ensin perinnön määrän Intian rupioissa.
@@ -141,7 +134,7 @@ void Tt5() {
 		Lopuksi vielä kysytään onko leski elossa(K / E tai Y / N).
 		Kun pohjatiedot on annettu, lasketaan ja tulostetaan paljonko kukin saa perintöä.Oletan, että Intiassa näin ei ole, mutta jaetaan rahat kaikkien kesken tasan.*/
 
-	int heritage;
+	float heritage;
 	int sons;
 	int daughters;
 	int widows;
@@ -164,31 +157,96 @@ void Tt5() {
 	cout << "Is widow alive?(Y/N)";
 	cin >> yn;
 	cout << endl;
-	if (yn == 'y' || yn == 'Y') { widows = 1; }
-	else if (yn == 'n' || yn == 'N') { widows = 0; }
-	else { cout << "invalid output" << endl; }
+	if ((yn == 'y') || (yn == 'Y')) { widows = 1; }
+	else if ((yn == 'n') || (yn == 'N')) { widows = 0; }
+	else {
+		cout << "invalid output" << endl;
+		widows = 1;
+	}
 
 
-	heritage = heritage / (sons + daughters + widows);
-	cout << "heritage:" << heritage;
+	heritage = heritage / (1 + (float)sons + (float)daughters + (float)widows);
+	cout << "heritage:" << heritage << endl;
 }
 
-void Vt1() {
-
+void ViikkoTeht1() {
+	cout << "Give a number";
+	int number;
+	cin >> number;
+	if (number > 0) { cout << "number is positive" << endl; }
+	else if (number < 0) { cout << "number is negative" << endl; }
+	else if (number == 0) { cout << "Number is not positive or negative" << endl; }
+	else { cout << "Input was not a number" << endl; }
 }
 
-void Vt2() {
-
+void ViikkoTeht2() {
+	cout << "Vt2" << endl;
+	int Kilometers;
+	cout << "Give distance in kilometers: ";
+	cin >> Kilometers;
+	cout << "---------------" << endl;
+	int Miles = Kilometers / 1.609;
+	int Seamiles = Kilometers / 1.852;
+	cout << "M - convert to miles" << endl;
+	cout << "S - convert to nautical miles" << endl;
+	cout << "Q - quit" << endl;
+	char inputMenu;
+	cin >> inputMenu;
+	if ((inputMenu == 'm') || (inputMenu == 'M')) { cout << "Miles:" << Miles << endl; }
+	else if ((inputMenu == 's') || (inputMenu == 'S')) { cout << "nautical miles: " << Seamiles << endl; }
+	else if ((inputMenu == 'q') || (inputMenu == 'Q')) { cout <<endl;  return; }
 }
 
-void Vt3() {
-
+void Viikkoteht3() {
+	cout << "Compute quotientand remainder :" << endl;
+	cout << "------------------------------------ -" << endl;
+	int divinder;
+	cout << "Input the dividend : ";
+	cin >> divinder;
+	cout << endl;
+	cout << "Input the divisor : ";
+	int divisor;
+	cin >> divisor;
+	cout << endl;
+	cout << "The quotient of the division is : " << (float)divinder / (float)divisor << endl;
+	cout << "The remainder of the division is : " << divinder % divisor << endl;
 }
 
-void Vt4() {
-
+void ViikkoTeht4() {
+	cout << "Vt4" << endl;
+	int year;
+	cout << "input a year: ";
+	cin >> year;
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		cout << "vuosi " << year << " on karkausvuosi" << endl;
+	}
+	else { cout << "vuosi " << year << " ei ole karkausvuosi" << endl; }
 }
 
-void Vt5() {
-
+void ViikkoTeht5() {
+	cout << "vt5" << endl;
+	int n1;
+	int n2;
+	int n3;
+	cout << "input number 1: ";
+	cin >> n1;
+	cout << "input number 2: ";
+	cin >> n2;
+	cout << "input number 3: ";
+	cin >> n3;
+	if ((n1 > n2) && (n1 > n3)) {
+		cout << "greatest number is " << n1 << endl;
+		if (n2 > n3) { cout << "smallest is " << n3 << endl; }
+		else { cout << "smallest is " << n2 << endl; }
+	}
+	else if (n2 > n3) {
+		cout << "greatest number is " << n2 << endl;
+		if (n1 > n3) { cout << "smallest number is " << n3 << endl; }
+		else { cout << "smallest number is " << n1 << endl; }
+	}
+	else {
+		cout << "greatest number is " << n3 << endl;
+		if (n1 > n2) { cout << "smallest number is " << n2; }
+		else { cout << "smallest number is " << n1; }
+	}
 }
